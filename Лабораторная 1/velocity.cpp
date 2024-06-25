2c9d02e6b678d0a506e53546bb22e5aee645f2a0
smik91 <125355749+smik91@users.noreply.github.com>
2024-06-25 22:43:24 +0300

    Лабораторная 1
    -PDF отчёт о лабораторной
    -Файл с исходным кодом

---
+++ Лабораторная 1/velocity.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+double calculate_velocity(double t) {
+    return 3 * t * t - 6 * t;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    double t;
+    cout << "Введите значение времени t: ";
+    cin >> t;
+    double velocity = calculate_velocity(t);
+    cout << "Скорость тела в момент времени t = " << t << ": " << velocity << endl;
+    return 0;
+}
